201b3d2ea73f3acbe53b52e2673865ca8bb6ac68
darcy <acednes@gmail.com>
2025-02-21 14:25:48 +1100

    refactor: create ui struct

--- src/main.zig
+++ src/main.zig
@@ -21,18 +21,6 @@ const Snap = struct {
     cursor: u32,
 };
 
-const InputBox = struct {
-    x: u16,
-    y: u16,
-    width: u16,
-};
-
-var input_box = InputBox{
-    .x = 2,
-    .y = 5,
-    .width = 40,
-};
-
 const keys = struct {
     const ESCAPE = 0x1b;
     const BACKSPACE = 0x107;
@@ -58,121 +46,152 @@ pub fn main() !void {
     state.snap.length = 6;
     state.snap.cursor = 6;
 
-    const stdscr = try curses.initscr();
+    const ui = try Ui.init();
+    defer ui.deinit();
 
-    try curses.noecho();
-    try stdscr.keypad(true);
-    try curses.set_escdelay(0);
+    while (true) {
+        try ui.frame(&state);
+    }
+}
 
-    defer {
+const Ui = struct {
+    window: curses.Window,
+
+    const InputBox = struct {
+        x: u16,
+        y: u16,
+        width: u16,
+    };
+    var input_box = InputBox{
+        .x = 2,
+        .y = 5,
+        .width = 40,
+    };
+
+    fn init() !Ui {
+        const window = try curses.initscr();
+
+        try curses.noecho();
+        try window.keypad(true);
+        try curses.set_escdelay(0);
+
+        return Ui{
+            .window = window,
+        };
+    }
+
+    fn deinit(self: Ui) void {
+        _ = self;
         curses.endwin() catch {};
     }
 
-    while (true) {
-        try frame(&stdscr, &state);
-    }
-}
+    fn frame(self: Ui, state: *State) !void {
+        const window = self.window;
 
-fn drawInputBox(stdscr: *const curses.Window) !void {
-    try stdscr.move(input_box.y, input_box.x);
-    try stdscr.addch('+');
-    for (0..input_box.width) |_| {
-        try stdscr.addch('-');
-    }
-    try stdscr.addch('+');
-    try stdscr.move(input_box.y + 1, input_box.x);
-    try stdscr.addch('|');
-    try stdscr.move(input_box.y + 1, input_box.x + input_box.width + 1);
-    try stdscr.addch('|');
-    try stdscr.move(input_box.y + 2, input_box.x);
-    try stdscr.addch('+');
-    for (0..input_box.width) |_| {
-        try stdscr.addch('-');
-    }
-    try stdscr.addch('+');
-}
+        try window.clear();
 
-fn frame(stdscr: *const curses.Window, state: *State) !void {
-    try stdscr.clear();
+        const size = try window.getScreenSize();
 
-    const size = try stdscr.getScreenSize();
+        try window.move(size.rows - 1, 0);
+        const mode = switch (state.mode) {
+            VimMode.Normal => "NORMAL",
+            VimMode.Insert => "Insert",
+        };
+        try window.addstr(mode);
 
-    try stdscr.move(size.rows - 1, 0);
-    const mode = switch (state.mode) {
-        VimMode.Normal => "NORMAL",
-        VimMode.Insert => "Insert",
-    };
-    try stdscr.addstr(mode);
+        try self.drawInputBox();
 
-    try drawInputBox(stdscr);
+        try window.move(
+            input_box.y + 2,
+            @intCast(@as(usize, input_box.x) + state.snap.cursor + 1),
+        );
+        try window.addch('^');
 
-    try stdscr.move(input_box.y + 2, @intCast(@as(usize, input_box.x) + state.snap.cursor + 1));
-    try stdscr.addch('^');
+        try window.move(input_box.y + 1, input_box.x + 1);
+        for (0..state.snap.length) |i| {
+            try window.addch(state.snap.buffer[i]);
+        }
 
-    try stdscr.move(input_box.y + 1, input_box.x + 1);
-    for (0..state.snap.length) |i| {
-        try stdscr.addch(state.snap.buffer[i]);
-    }
+        const key = try window.getch();
+        switch (state.mode) {
+            VimMode.Normal => {
+                switch (key) {
+                    'q' => {
+                        try curses.endwin();
+                        std.process.exit(0);
+                    },
 
-    const key = try stdscr.getch();
+                    'i' => {
+                        state.mode = .Insert;
+                    },
 
-    switch (state.mode) {
-        VimMode.Normal => {
-            switch (key) {
-                'q' => {
-                    try curses.endwin();
-                    std.process.exit(0);
-                },
+                    else => {},
+                }
+            },
 
-                'i' => {
-                    state.mode = .Insert;
-                },
+            VimMode.Insert => {
+                switch (key) {
+                    keys.ESCAPE => {
+                        state.mode = .Normal;
+                    },
 
-                else => {},
-            }
-        },
-
-        VimMode.Insert => {
-            switch (key) {
-                keys.ESCAPE => {
-                    state.mode = .Normal;
-                },
-
-                keys.BACKSPACE => {
-                    if (state.snap.cursor > 0 and state.snap.length > 0) {
-                        for (state.snap.cursor..state.snap.length) |i| {
-                            state.snap.buffer[i - 1] = state.snap.buffer[i];
+                    keys.BACKSPACE => {
+                        if (state.snap.cursor > 0 and state.snap.length > 0) {
+                            for (state.snap.cursor..state.snap.length) |i| {
+                                state.snap.buffer[i - 1] = state.snap.buffer[i];
+                            }
+                            state.snap.cursor -= 1;
+                            state.snap.length -= 1;
                         }
-                        state.snap.cursor -= 1;
-                        state.snap.length -= 1;
-                    }
-                },
+                    },
 
-                keys.ARROW_LEFT => {
-                    if (state.snap.cursor > 0) {
-                        state.snap.cursor -= 1;
-                    }
-                },
-                keys.ARROW_RIGHT => {
-                    if (state.snap.cursor < state.snap.length) {
-                        state.snap.cursor += 1;
-                    }
-                },
-
-                keys.PRINTABLE_START...keys.PRINTABLE_END => {
-                    if (state.snap.length < MAX_INPUT) {
-                        var i = state.snap.length;
-                        while (i > state.snap.cursor) : (i -= 1) {
-                            state.snap.buffer[i] = state.snap.buffer[i - 1];
+                    keys.ARROW_LEFT => {
+                        if (state.snap.cursor > 0) {
+                            state.snap.cursor -= 1;
                         }
-                        state.snap.buffer[state.snap.cursor] = @intCast(key);
-                        state.snap.cursor += 1;
-                        state.snap.length += 1;
-                    }
-                },
+                    },
+                    keys.ARROW_RIGHT => {
+                        if (state.snap.cursor < state.snap.length) {
+                            state.snap.cursor += 1;
+                        }
+                    },
 
-                else => {},
-            }
-        },
+                    keys.PRINTABLE_START...keys.PRINTABLE_END => {
+                        if (state.snap.length < MAX_INPUT) {
+                            var i = state.snap.length;
+                            while (i > state.snap.cursor) : (i -= 1) {
+                                state.snap.buffer[i] = state.snap.buffer[i - 1];
+                            }
+                            state.snap.buffer[state.snap.cursor] = @intCast(key);
+                            state.snap.cursor += 1;
+                            state.snap.length += 1;
+                        }
+                    },
+
+                    else => {},
+                }
+            },
+        }
     }
-}
+
+    fn drawInputBox(self: Ui) !void {
+        const window = self.window;
+
+        try window.move(input_box.y, input_box.x);
+        try window.addch('+');
+        for (0..input_box.width) |_| {
+            try window.addch('-');
+        }
+        try window.addch('+');
+        try window.move(input_box.y + 1, input_box.x);
+        try window.addch('|');
+        try window.move(input_box.y + 1, input_box.x + input_box.width + 1);
+        try window.addch('|');
+        try window.move(input_box.y + 2, input_box.x);
+        try window.addch('+');
+        for (0..input_box.width) |_| {
+            try window.addch('-');
+        }
+        try window.addch('+');
+    }
+};
